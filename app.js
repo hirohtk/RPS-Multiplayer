244ad7ce4c31486d115ef81e627b0f6e9bd5aaee
Kensen <hirohtk@gmail.com>
2019-10-18 18:48:59 -0700

    added confirm button function

--- app.js
+++ app.js
@@ -22,68 +22,109 @@ $(document).ready(function () {
     var database = firebase.database();
 
     var newGame = true;
-    window.snapshotKey;
+    //window.snapshotKey;
+
+
 
     $(".selectionButtonP1").on("click", function (event) {
         event.preventDefault();
-        if ($(this).attr("id") === "rock-button1" && newGame) {
-            console.log("Rock has been chosen");
-            var choice = "rock";
+
+        function newGamePush() {
             database.ref().push({
                 choice: choice,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
             });
             newGame = false;
-
-            database.ref().on("child_added", function (snapshot) { 
-                console.log(snapshot.key);
-                var snapshotKey = snapshot.key;
-                window.snapshotKey = snapshotKey; // maybe I can use this?  this sets the snapshot key to a global variable, supposedly so I can call it below. 
-            });
+            $(".player-one-selection").append("<h3 style='color:green';>Confirmed!</h3>")
         }
-        else if ($(this).attr("id") === "rock-button1" && !newGame) {
-            console.log("rock has been chosen (2nd choice)");
-            var choice = "rock";
+        
+        function notNewGameSet() {
             database.ref().set({
                 choice: choice,
                 dateAdded: firebase.database.ServerValue.TIMESTAMP
             });
-            console.log(window.snapshotKey);
+            $(".player-one-selection").append("<h3 style='color:green';>Confirmed!</h3>")
+        }
+
+        if ($(this).attr("id") === "rock-button1" && newGame) {
+            console.log("Rock has been chosen");
+            var choice = "rock";
+            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $("#submit-button").on("click", function (event) {
+                event.preventDefault();
+                newGamePush();
+            });
+
+            database.ref().on("child_added", function (snapshot) { 
+                console.log("snapshot key is " + snapshot.key);
+                var snapshotKey = snapshot.key;
+                window.snapshotKey = snapshotKey; // maybe I can use this?  this sets the snapshot key to a global variable, supposedly so I can call it below. 
+            });
+        }
+        else if ($(this).attr("id") === "rock-button1" && newGame === false) {
+            console.log("rock has been chosen (2nd choice)");
+            var choice = "rock";
+
+            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $("#submit-button").on("click", function (event) {
+                event.preventDefault();
+                notNewGameSet();
+            });
+            
+            console.log("snapshot key is " + window.snapshotKey);
 
         }
         else if ($(this).attr("id") === "paper-button1" && newGame) {
             console.log("Paper has been chosen");
             var choice = "paper";
-            database.ref().push({
-                choice: choice,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $("#submit-button").on("click", function (event) {
+                event.preventDefault();
+                newGamePush();
             });
-            newGame = false;
+
+            database.ref().on("child_added", function (snapshot) { 
+                console.log("snapshot key is " + snapshot.key);
+                var snapshotKey = snapshot.key;
+                window.snapshotKey = snapshotKey; // maybe I can use this?  this sets the snapshot key to a global variable, supposedly so I can call it below. 
+            });
         }
-        else if ($(this).attr("id") === "paper-button1" && !newGame) {
+        else if ($(this).attr("id") === "paper-button1" && newGame === false) {
             console.log("Paper has been chosen (2nd choice)");
             var choice = "paper";
-            database.ref().set({
-                choice: choice,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+
+            $("#submit-button").on("click", function (event) {
+                event.preventDefault();
+                notNewGameSet();
             });
+            console.log("snapshot key is " + window.snapshotKey);
         }
         else if ($(this).attr("id") === "scissors-button1" && newGame){
             console.log("Scissors has been chosen");
             var choice = "scissors";
-            database.ref().push({
-                choice: choice,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $("#submit-button").on("click", function (event) {
+                event.preventDefault();
+                newGamePush();
             });
-            newGame = false;
+            
+            database.ref().on("child_added", function (snapshot) { 
+                console.log("snapshot key is " + snapshot.key);
+                var snapshotKey = snapshot.key;
+                window.snapshotKey = snapshotKey; // maybe I can use this?  this sets the snapshot key to a global variable, supposedly so I can call it below. 
+            });
         }
-        else if ($(this).attr("id") === "scissors-button1" && !newGame){
+        else if ($(this).attr("id") === "scissors-button1" && newGame === false){
             console.log("Scissors has been chosen (2nd choice)");
             var choice = "scissors";
-            database.ref().set({
-                choice: choice,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $("#submit-button").on("click", function (event) {
+                event.preventDefault();
+                notNewGameSet();
             });
+            console.log("snapshot key is " + window.snapshotKey);
             
         }  
 

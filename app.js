2d341ae601097d62174c844a2d9ef3fbaf80f4ef
Kensen <hirohtk@gmail.com>
2019-10-18 19:15:52 -0700

    last commit had a bug.  needed to clean up code in order for the submit button to work.  essentially, had the submit button under the same event listener as the button choice, needed to take it out, and put it on the same local scope as button choice

--- app.js
+++ app.js
@@ -24,19 +24,60 @@ $(document).ready(function () {
     var newGame = true;
     //window.snapshotKey;
 
+    var player1Wins = 0;
+    var player2Wins = 0;
 
+//Player one
+    firstGame();
+    
+    //nextGames();
 
-    $(".selectionButtonP1").on("click", function (event) {
-        event.preventDefault();
-
+    function firstGame() {
+        var choice; 
+        
         function newGamePush() {
             database.ref().push({
                 choice: choice,
             });
             newGame = false;
             $(".player-one-selection").append("<h3 style='color:green';>Confirmed!</h3>")
+
+            database.ref().on("child_added", function (snapshot) {
+                console.log("snapshot key is " + snapshot.key);
+                var snapshotKey = snapshot.key;
+                window.snapshotKey = snapshotKey; 
+            });
         }
-        
+
+        $(".selectionButtonP1").on("click", function (event) {
+            event.preventDefault();
+
+            if ($(this).attr("id") === "rock-button1") {
+                console.log("Rock has been chosen");
+                choice = "rock";
+                $(".player-one-selection").html("<h3>Player one choice: " + choice + "</h3>")
+            }
+            else if ($(this).attr("id") === "paper-button1") {
+                console.log("Paper has been chosen");
+                choice = "paper";
+                $(".player-one-selection").html("<h3>Player one choice: " + choice + "</h3>")
+            }
+            else if ($(this).attr("id") === "scissors-button1") {
+                console.log("Scissors has been chosen");
+                choice = "scissors";
+                $(".player-one-selection").html("<h3>Player one choice: " + choice + "</h3>")
+            }
+            // USER SHOULD BE ABLE TO KEEP CLICKING TO CHANGE THEIR MIND, NEVER GETS TO EXECUTE NEWGAMEPUSH UNLESS SUBMITBUTTON IS CLICKED
+
+        });
+
+        $("#submit-button").on("click", function (event) {
+            event.preventDefault();
+            newGamePush();
+        });
+    }
+    
+    function nextGames() {
         function notNewGameSet() {
             database.ref().set({
                 choice: choice,
@@ -45,54 +86,25 @@ $(document).ready(function () {
             $(".player-one-selection").append("<h3 style='color:green';>Confirmed!</h3>")
         }
 
-        if ($(this).attr("id") === "rock-button1" && newGame) {
-            console.log("Rock has been chosen");
-            var choice = "rock";
-            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
-            $("#submit-button").on("click", function (event) {
-                event.preventDefault();
-                newGamePush();
-            });
-
-            database.ref().on("child_added", function (snapshot) { 
-                console.log("snapshot key is " + snapshot.key);
-                var snapshotKey = snapshot.key;
-                window.snapshotKey = snapshotKey; // maybe I can use this?  this sets the snapshot key to a global variable, supposedly so I can call it below. 
-            });
-        }
-        else if ($(this).attr("id") === "rock-button1" && newGame === false) {
+        if ($(this).attr("id") === "rock-button1") {
             console.log("rock has been chosen (2nd choice)");
             var choice = "rock";
 
-            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $(".player-one-selection").html("<h3>Player one choice: " + choice + "</h3>")
             $("#submit-button").on("click", function (event) {
                 event.preventDefault();
                 notNewGameSet();
             });
-            
+
             console.log("snapshot key is " + window.snapshotKey);
 
         }
-        else if ($(this).attr("id") === "paper-button1" && newGame) {
-            console.log("Paper has been chosen");
-            var choice = "paper";
-            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
-            $("#submit-button").on("click", function (event) {
-                event.preventDefault();
-                newGamePush();
-            });
 
-            database.ref().on("child_added", function (snapshot) { 
-                console.log("snapshot key is " + snapshot.key);
-                var snapshotKey = snapshot.key;
-                window.snapshotKey = snapshotKey; // maybe I can use this?  this sets the snapshot key to a global variable, supposedly so I can call it below. 
-            });
-        }
-        else if ($(this).attr("id") === "paper-button1" && newGame === false) {
+        else if ($(this).attr("id") === "paper-button1") {
             console.log("Paper has been chosen (2nd choice)");
             var choice = "paper";
 
-            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $(".player-one-selection").html("<h3>Player one choice: " + choice + "</h3>")
 
             $("#submit-button").on("click", function (event) {
                 event.preventDefault();
@@ -100,38 +112,31 @@ $(document).ready(function () {
             });
             console.log("snapshot key is " + window.snapshotKey);
         }
-        else if ($(this).attr("id") === "scissors-button1" && newGame){
-            console.log("Scissors has been chosen");
-            var choice = "scissors";
-            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
-            $("#submit-button").on("click", function (event) {
-                event.preventDefault();
-                newGamePush();
-            });
-            
-            database.ref().on("child_added", function (snapshot) { 
-                console.log("snapshot key is " + snapshot.key);
-                var snapshotKey = snapshot.key;
-                window.snapshotKey = snapshotKey; // maybe I can use this?  this sets the snapshot key to a global variable, supposedly so I can call it below. 
-            });
-        }
-        else if ($(this).attr("id") === "scissors-button1" && newGame === false){
+
+        else if ($(this).attr("id") === "scissors-button1") {
             console.log("Scissors has been chosen (2nd choice)");
             var choice = "scissors";
 
-            $(".player-one-selection").html("<h3>Player one choice: "+ choice + "</h3>")
+            $(".player-one-selection").html("<h3>Player one choice: " + choice + "</h3>")
             $("#submit-button").on("click", function (event) {
                 event.preventDefault();
                 notNewGameSet();
             });
             console.log("snapshot key is " + window.snapshotKey);
-            
-        }  
 
+        }
+    }
+    
         
-    });
 
-/*  */
+
+
+
+
+    /*  */
 
 });
+
+
+
 
